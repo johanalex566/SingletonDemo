3132c986ddfbd10d970faf73c21bb897e0acc576
johanalex566 <johanalex566@gmail.com>
2022-10-03 21:38:39 -0500

    Add project files.

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySingleton mySingleton = MySingleton.Instance;
+            mySingleton.DoSomething();
+        }
+    }
+
+    public class MySingleton
+    {
+        private static MySingleton instance = null;
+        public static MySingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MySingleton();
+                }
+                return instance;
+            }
+        }
+
+        private MySingleton()
+        {
+
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Singleton class code has been called");
+        }
+    }
+}
